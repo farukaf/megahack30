18b1bca2f2a476ce71057d9c1959d4e983e69918
Faruk Abdo Feres <faruk_feres@hotmail.com>
2020-06-29 23:45:11 -0300

    Icons change

---
+++ BotekoGO/ButekoGOAPP/ButekoGOAPP.Android/TodoShellRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ButekoGOAPP.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Shell), typeof(TodoShellRenderer))]
+namespace ButekoGOAPP.Droid
+{
+    public class TodoShellRenderer : ShellRenderer
+    {
+        public TodoShellRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override IShellItemRenderer CreateShellItemRenderer(ShellItem shellItem)
+        {
+            return new TodoShellItemRenderer(this);
+        }
+    }
+
+}
---
+++ BotekoGO/ButekoGOAPP/ButekoGOAPP/ViewModels/TodoTabBar.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+
+namespace ButekoGOAPP.ViewModels
+{
+    public class TodoTabBar : TabBar
+    {
+        public Tab LargeTab { get; set; }
+    }
+}
